d28911abfc4abc03eb2d1517fefd56acb6d7dc88
Syver Enstad <syver.enstad@gmail.com>
2016-05-24 15:15:06 +0200

    CRU(-D) implemented for users.

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Rumbl.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
+     {:mix_test_watch, "~> 0.2", only: [:dev, :test]},
      {:cowboy, "~> 1.0"}]
   end
 
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -34,7 +34,7 @@ defmodule Rumbl.ConnCase do
 
   setup tags do
     unless tags[:async] do
-      #Ecto.Adapters.SQL.restart_test_transaction(Rumbl.Repo, [])
+      Ecto.Adapters.SQL.restart_test_transaction(Rumbl.Repo, [])
     end
 
     {:ok, conn: Phoenix.ConnTest.conn()}
