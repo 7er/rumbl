1297ba3dda5fd4208e7771c85a286a460d1e0612
Syver Enstad <syver.enstad@gmail.com>
2016-05-31 16:17:10 +0200

    Port Counter from GenServer to Agent
    Quite a bit less code as we don't have to implement
    the server callbacks
    
    - We haven't got the timer tick anymore

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -1,37 +1,8 @@
-defmodule Rumbl.Counter do
-  use GenServer
-  
-  def inc(pid), do: GenServer.cast(pid, :inc)
-  def dec(pid), do: GenServer.cast(pid, :dec)
-  def val(pid) do
-    GenServer.call(pid, :val)
-  end
-
+defmodule Rumbl.Counter do  
+  def inc(pid), do: Agent.update(pid, &(&1 + 1))
+  def dec(pid), do: Agent.update(pid, &(&1 - 1))
+  def val(pid), do: Agent.get(pid, &(&1))
   def start_link(initial_val) do    
-    GenServer.start_link(__MODULE__, initial_val)
-  end
-
-  def init(initial_val) do
-    Process.send_after(self, :tick, 1000)
-    {:ok, initial_val}
-  end
-
-  def handle_cast(:inc, val) do
-    {:noreply, val + 1}
-  end
-
-  def handle_cast(:dec, val) do
-    {:noreply, val - 1}
-  end
-
-  def handle_call(:val, _from, val) do
-    {:reply, val, val}
-  end
-
-  def handle_info(:tick, 0), do: raise "boom!"
-  def handle_info(:tick, val) do
-    IO.puts "tick #{val}"
-    Process.send_after(self, :tick, 1000)
-    {:noreply, val - 1}
+    Agent.start_link(fn -> initial_val end)
   end
 end
